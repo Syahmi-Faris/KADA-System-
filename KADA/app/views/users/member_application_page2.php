8cd9748b02696b9deee962e433f7f9e1915e57a5
Syahmi Faris <147996757+Syahmi-Faris@users.noreply.github.com>
2025-01-01 11:00:28 +0800

    Delete KADA directory

--- KADA/app/views/users/member_application_page2.php
+++
@@ -1,9 +0,0 @@
-<form action="/member-application/page3" method="POST">
-    <label>Nama Pewaris:</label> 
-    <input type="text" name="nama_pewaris" required><br>
-    <label>Hubungan:</label> 
-    <input type="text" name="hubungan_pewaris" required><br>
-    <label>No. K/P Pewaris:</label> 
-    <input type="text" name="no_kp_pewaris" required><br>
-    <button type="submit">Next</button>
-</form>
